3c7ef5c2dfc4869f308d46a03dc28252189badc7
ErlendAbrahamsen <erlend_abrahamsen@hotmail.com>
2019-10-17 19:09:49 +0200

    Start of neural network part c

---
+++ Project2/Source_code/Part_c.py
@@ -0,0 +1,25 @@
+from Part_a import X, y
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+def backPropagation():
+    """
+    """
+
+
+
+    return None
+
+def gradientDecent():
+    """
+    """
+
+
+
+    return None
+
+if __name__ == '__main__':
+    pass
