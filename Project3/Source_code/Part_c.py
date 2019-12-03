c814a384e392510fd38e1e15addbb4a8d830fa4b
ErlendAbrahamsen <erlend_abrahamsen@hotmail.com>
2019-12-03 22:38:17 +0100

    feed_forward() and loss() update

--- Project3/Source_code/Part_c.py
+++ Project3/Source_code/Part_c.py
@@ -2,6 +2,7 @@ from Part_b import u
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import tensorflow as tf
 from tensorflow import keras
 
@@ -29,40 +30,47 @@ class HeatLearner:
 
             self.weights, self.biases = weights, biases
 
-    def feed_forward(self):
+    def feed_forward(self, x, t):
         """
         """
 
         num_layers = len(self.layer_dimensions)
-        a = tf.concat([self.x, self.t], 1)
+        a = tf.concat([x, t], 1)
         for l in range(num_layers-2):
             W, b = self.weights[l], self.biases[l]
-            a = tf.add(tf.matmul(a, W), b)
+            a = tf.nn.sigmoid(tf.add(tf.matmul(a, W), b))
 
-            W, b = self.weights[-1], self.biases[-1]
-            self.dnn_out = tf.add(tf.matmul(a, W), b)
+        W, b = self.weights[-1], self.biases[-1]
+        self.dnn_out = tf.add(tf.matmul(a, W), b)
+
+        return self.dnn_out
 
     def loss(self):
         """
         """
 
-        self.feed_forward()
-        u_hat = self.dnn_out**1.0
+        u_hat = self.feed_forward(self.x, self.t)**1.0
+
+        #Parital derivatives
         u_x = tf.gradients(u_hat, self.x)[0]
         u_xx = tf.gradients(u_x, self.x)[0]
         u_t = tf.gradients(u_hat, self.t)[0]
 
+        #Initial and boundary conditions
+        zeros = tf.zeros(tf.shape(self.x), dtype=tf.float64)
+        u_t0 = self.feed_forward(self.x, zeros)     #t=0
+        u_x0 = self.feed_forward(zeros, self.t)     #x=0
+        u_x1 = self.feed_forward(zeros+1, self.t)   #x=1
 
+        terms = [u_t-u_xx, u_t0-tf.sin(np.pi*self.x), u_x0, u_x1]
+        return 1/36*(tf.reduce_sum(tf.square(terms[0])) + tf.reduce_sum(tf.square(terms[1])) + tf.reduce_sum(tf.square(terms[2])) + tf.reduce_sum(tf.square(terms[3])))
 
-        #u_t0 = feed_forward(x, np.zeros((36,1)), layer_dimensions, weights, biases)
-        #u_x0 = feed_forward(np.zeros((36,1)), t, layer_dimensions, weights, biases)
-        #u_x1 = feed_forward(np.ones((36,1)), t, layer_dimensions, weights, biases)
-        u_t0 = u_hat[:6]
-        diff = u_t0 - tf.sin(np.pi*self.x[:6])
+        #diff = u_t0 - tf.sin(np.pi*self.x[:6])
 
-        return 1/36*(tf.reduce_sum(tf.square(u_t-u_xx)) + tf.reduce_sum(tf.square(diff)))
+        #return 1/36*(tf.reduce_sum(tf.square(u_t-u_xx)))# + tf.reduce_sum(tf.square(diff)))
         #zeros = tf.zeros(tf.shape(x), dtype=tf.float64)
-        #return keras.losses.MSE(zeros, u_t-u_xx) + keras.losses.MSE(zeros, diff)
+        #return keras.losses.MSE(zeros, u_t-u_xx) #+ keras.losses.MSE(zeros, diff)
+        #print(tf.shape(zeros), tf.shape(u_t-u_xx))
 
     def train(self, num_iter=100):
         """
@@ -78,14 +86,18 @@ class HeatLearner:
                 sess.run(optimizer)
 
             print("Final loss:", self.loss().eval())
+            return self.dnn_out.eval()
+
+    #def predict(self, )
 
 if __name__ == '__main__':
     tf.set_random_seed(1)
     n = 6
     x = np.linspace(0, 1, n)
     x, t = np.meshgrid(x, x)
-    layer_dimensions = [2, 25, 25, 2]
-    num_iter = 300
+    layer_dimensions = [2, 100, 100, 100, 1]
 
     dnn = HeatLearner(x, t, layer_dimensions)
-    dnn.train(num_iter=300)
+    out = dnn.train(num_iter=300)
+    true = u(x.reshape(-1, 1), t.reshape(-1, 1))
+    print(mean_squared_error(np.ravel(true), np.ravel(out)))
