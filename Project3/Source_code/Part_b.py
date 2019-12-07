5e02cef0ca1c245b1fbc005f54b8554e6bcd3755
ErlendAbrahamsen <erlend_abrahamsen@hotmail.com>
2019-12-07 16:12:40 +0100

    Updatet ratio anlysis with grid_analysis() instead

--- Project3/Source_code/Part_b.py
+++ Project3/Source_code/Part_b.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
 from sklearn.metrics import mean_squared_error
 
 def MyPlot(x, y, z, title="", shrink=0.5, aspect=4):
@@ -22,14 +23,14 @@ def MyPlot(x, y, z, title="", shrink=0.5, aspect=4):
 
     return True
 
-def finit_diff(dx=1e-1, ratio=0.5):
+def finit_diff(dx=1e-1, ratio=0.5, t_stop=1):
     """
     Forward time centered space finit differences of
     simple 1d heat eq /w given conditions.
     Returns 1d-arrays x, y and 2d-array u.
     """
 
-    x_stop, t_stop = 1, 2
+    x_stop, t_stop = 1, t_stop
     dt = ratio*dx**2
     d = dt/dx**2
     d2 = 1 - 2*d
@@ -46,23 +47,21 @@ def finit_diff(dx=1e-1, ratio=0.5):
 
     return x, t, u
 
-def stability_ratio_error_analysis(n=5, max_ratio=0.5, dx=1e-1):
+def grid_analysis(t_stops, ratios, dx=1e-1):
     """
-    Computes MSE(analytic, finit_diff) for
-    ratio values [max_ratio/n, ..., max_ratio].
-    Returns list ratios and MSE.
+    Returns ndarray of MSE(u_analytic, u_approx) for different
+    dt/dx^2 ratios and t_stops end times.
     """
 
-    MSE = []
-    ratios = np.linspace(max_ratio/n, max_ratio, n)
-    for ratio in ratios:
-        x, t, u_approx = finit_diff(dx=dx, ratio=ratio)
-        x_, t_ = np.meshgrid(x, t)
-        u_analytic = u_true(x_, t_).T
+    MSE = np.zeros((len(t_stops), len(ratios)))
+    for i, t_stop in enumerate(t_stops):
+        for j, ratio in enumerate(ratios):
+            x, t, u_approx = finit_diff(dx=dx, ratio=ratio, t_stop=t_stop)
+            x_, t_ = np.meshgrid(x, t)
+            u_analytic = u_true(x_, t_).T
+            MSE[i, j] = mean_squared_error(np.ravel(u_analytic), np.ravel(u_approx))
 
-        MSE.append(mean_squared_error(np.ravel(u_analytic), np.ravel(u_approx)))
-
-    return ratios, MSE
+    return 10**3*MSE
 
 def u_true(x, t):
     """
@@ -73,41 +72,39 @@ def u_true(x, t):
     return np.exp(-np.pi**2*t)*np.sin(np.pi*x)
 
 if __name__ == '__main__':
-    ### MSE for different ratios (dt/dx^2 = ratio) ###
+    #### Grid analysis plot ###
     dx1, dx2 = 1e-1, 1e-2
-    ratios, MSE1 = stability_ratio_error_analysis(n=5, dx=dx1)
-    plt.title("$MSE$ errors for different ratios")
-    plt.plot(ratios, 1e3*np.array(MSE1), label="$\Delta x = %.1e$" % dx1)
-    plt.xlabel("$=\Delta t / \Delta x^2$"), plt.ylabel("$10^{-3}$ $MSE$")
+    t_stops = [0.5, 1.0, 1.5, 2.0]
+    ratios = [0.2, 0.3, 0.4, 0.5]
+
+    MSE = grid_analysis(t_stops, ratios, dx=dx1)
+    fig, ax = plt.subplots()
+    sns.heatmap(MSE, annot=True, ax=ax, cmap="viridis", fmt=".3g",
+                xticklabels=ratios, yticklabels=t_stops)
+    ax.set_title("$10^{-3}$ $MSE_{total}$")
+    ax.set_xlabel("$\Delta t / \Delta x^2$"), ax.set_ylabel("$t_{stop}$")
     plt.show()
 
-    ratios, MSE2 = stability_ratio_error_analysis(max_ratio=0.6, n=6, dx=dx1)
-    plt.title("Breaking stability criterion")
-    plt.plot(ratios, np.array(MSE2), label="$\Delta x = %.1e$" % dx1)
-    plt.xlabel("$=\Delta t / \Delta x^2$"), plt.ylabel("$MSE$")
-    plt.show()
-
-    ### Analytic and finit diff start state (t=0) ###
+    ### Analytic vs finit-diff at two fixed times###
     dx = dx1
-    x, t, u_approx = finit_diff(dx=dx)
+    x, t, u_approx = finit_diff(dx=dx, ratio=0.5, t_stop=2)
     x_, t_ = np.meshgrid(x, t)
     u_analytic = u_true(x_, t_).T
 
     mse = mean_squared_error(np.ravel(u_analytic), np.ravel(u_approx))
 
-    t_index = 0
-    plt.title("Start-state $(t=%.1f)$ with $\Delta x = %.1e$" % (t[t_index], dx))
+    t_index = 5
+    plt.title("Start-state $(t=%.2f)$ with $\Delta x = %.1e$" % (t[t_index], dx))
     plt.plot(x, u_approx[:, t_index], label="finit-diff")
-    plt.plot(x, u_analytic[:, t_index], "--", color="r", label="analytic-solution")
+    plt.plot(x, u_analytic[:, t_index], ".", color="r", label="analytic-solution")
     plt.xlabel("x-axis"), plt.ylabel("u-axis"), plt.text(0.35, 0, ("$MSE_{total}=%.2e$" % mse))
     plt.xlim(-0.05, 1.05), plt.ylim(-0.05, 1.05)
     plt.legend(), plt.show()
 
-    ### Analytic vs finit-diff at fixed time###
-    t_index = 5
+    t_index = 45
     plt.title("$t=%.2f$ with $\Delta x = %.1e$" % (t[t_index], dx))
     plt.plot(x, u_approx[:, t_index], label="Finit-diff")
-    plt.plot(x, u_analytic[:, t_index], "--", color="r", label="True-value")
+    plt.plot(x, u_analytic[:, t_index], ".", color="r", label="True-value")
     plt.xlabel("x-axis"), plt.ylabel("u-axis"), plt.text(0.35, 0.3, ("$MSE_{total}=%.2e$" % mse))
     plt.xlim(-0.05, 1.05), plt.ylim(-0.05, 1.05)
     plt.legend(), plt.show()
