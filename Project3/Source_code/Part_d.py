ed080e0c40cedd0d7c46ba6ef157bfadb82e7da1
ErlendAbrahamsen <erlend_abrahamsen@hotmail.com>
2019-12-11 02:57:43 +0100

    small updates

--- Project3/Source_code/Part_d.py
+++ Project3/Source_code/Part_d.py
@@ -6,8 +6,11 @@ import tensorflow as tf
 from tensorflow.losses import mean_squared_error
 from tensorflow import keras
 
-def eigen_ODE_loss(x0, t, x_hat, l=300):
+seed = 1
+n, m = 2, 10
+def eigen_ODE_loss(x0, t, x_hat, l=n*m, seed=seed):
     """
+    (l=n*m)
     Custom loss function to be used in HeatLearner class.
     ( HeatLearner(custom_loss=eigen_ODE_loss) )
 
@@ -16,8 +19,7 @@ def eigen_ODE_loss(x0, t, x_hat, l=300):
     x(t=t_m) is shape (n, 1).
     """
 
-    np.random.seed(1) ###
-    n = 6
+    np.random.seed(seed)
     A = np.random.normal(0, 1, (n, n))
     A = (A.T + A)/2
     A = tf.convert_to_tensor(A, dtype=tf.float64) #(n, n) symmetrical matrix
@@ -44,21 +46,22 @@ def eigen_ODE_loss(x0, t, x_hat, l=300):
     return mean_squared_error(x_t, fx-x)
 
 if __name__ == '__main__':
-    tf.set_random_seed(1), np.random.seed(1) ###
-    n, m = 6, 50
+    tf.set_random_seed(1)
     x0 = np.random.normal(0, 1, n)  #x0(t) guess
-    t = np.linspace(0, 100, m)      #times
+    t = np.linspace(0, 1, m)      #times
     x0, t = np.meshgrid(x0, t)
 
     #Random symmetrical (n, n) (normal distb.)
+    np.random.seed(seed)
     A = np.random.normal(0, 1, (n, n))
     A = (A.T + A)/2
     eig_val, eig_vec = np.linalg.eig(A)
 
     #Layer architecture and training
-    layer_dimensions = [2, 200, 200, 1]
-    dnn = HeatLearner(x0, t, layer_dimensions, activation=tf.nn.sigmoid, custom_loss=eigen_ODE_loss)
-    dnn.train(num_iter=200)
+    layer_dimensions = [2, 50, 50, 50, 50, 1]
+    dnn = HeatLearner(x0, t, layer_dimensions, activation=tf.nn.tanh, out=tf.nn.tanh,
+                      custom_loss=eigen_ODE_loss, optimizer="SGD", eta=1e-10)
+    dnn.train(num_iter=100)
     x = dnn.predict(x0, t)
 
     ### Compute lambda at each timepoint t_m ###
@@ -70,8 +73,9 @@ if __name__ == '__main__':
 
     ### Final plotting of each true eigenval and predicted eigenval. ###
     abs_end_error = min([abs(Lmbd[-1]-eig) for eig in eig_val])
-    plt.title("ODE predicted $\lambda$ vs Numpy computed $\lambda$")
+    plt.title("ODE predicted $\lambda$ vs Numpy computed $\lambda$ \n $random_{seed}=%d$" % seed)
     plt.plot(Lmbd, label="Predicted $\lambda$")
     plt.plot(eig_val, "o", c="r", label="True $\lambda$")
-    plt.text(7.5, 1, ("$Error_{end}=%.2e$" % abs_end_error))
+    plt.text(4, 0.4, ("$Error_{end}=%.2e$" % abs_end_error))
+    plt.xlabel("time steps"), plt.ylabel("Eig. value $\lambda$")
     plt.legend(), plt.show()
